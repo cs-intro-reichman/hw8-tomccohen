50e527ef476c3d161fbd4ceaa3ca17098e1916fd
Tom Cohen <tom.cohen03@post.runi.ac.il>
2026-01-03 21:28:15 +0200

    Network implementation

--- Network.java
+++ Network.java
@@ -135,11 +135,11 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-       String ans = "";
+       String ans ="Network: ";
 
        for (int i = 0; i < userCount; i++)
        {
-            ans += users[i].toString() + "\n";
+            ans += "\n" + users[i].toString();
        }
        return ans;
     }
